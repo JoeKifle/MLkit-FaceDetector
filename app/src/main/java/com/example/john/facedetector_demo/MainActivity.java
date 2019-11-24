725995bd47206c0c110385345ea2e55736abc624
@joekifle <joey.kifle@gmail.com>
2019-11-24 16:03:31 +0300

    Add Firebase Facedetector class

--- app/src/main/java/com/example/john/facedetector_demo/MainActivity.java
+++ app/src/main/java/com/example/john/facedetector_demo/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.john.facedetector_demo;
 
 import android.graphics.Bitmap;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.john.facedetector_demo.Helper.GraphicOverlay;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.face.FirebaseVisionFace;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetector;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions;
 import com.wonderkiln.camerakit.CameraKitError;
 import com.wonderkiln.camerakit.CameraKitEvent;
 import com.wonderkiln.camerakit.CameraKitEventListener;
@@ -15,9 +24,17 @@ import com.wonderkiln.camerakit.CameraKitImage;
 import com.wonderkiln.camerakit.CameraKitVideo;
 import com.wonderkiln.camerakit.CameraView;
 
+import java.util.List;
+
 import dmax.dialog.SpotsDialog;
 
 public class MainActivity extends AppCompatActivity {
+
+    CameraView cameraView;
+    GraphicOverlay graphicOverlay;
+    Button btnDetect;
+
+    android.app.AlertDialog waitingDialog;
 
 
 
@@ -27,5 +44,102 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // init view
+
+        cameraView = (CameraView) findViewById(R.id.camera_view);
+        graphicOverlay = (GraphicOverlay) findViewById(R.id.graphic_overlay);
+        btnDetect = (Button) findViewById(R.id.detectFaces);
+
+        waitingDialog = new SpotsDialog.Builder().setContext(this)
+                .setMessage("Please Wait")
+                .setCancelable(false)
+                .build();
+
+
+        // click event listener
+
+        btnDetect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                cameraView.start();
+                cameraView.captureImage();
+                graphicOverlay.clear();
+
+            }
+        });
+
+        cameraView.addCameraKitListener(new CameraKitEventListener() {
+            @Override
+            public void onEvent(CameraKitEvent cameraKitEvent) {
+
+            }
+
+            @Override
+            public void onError(CameraKitError cameraKitError) {
+
+            }
+
+            @Override
+            public void onImage(CameraKitImage cameraKitImage) {
+
+                waitingDialog.show();
+                Bitmap bitmap = cameraKitImage.getBitmap();
+                bitmap = Bitmap.createScaledBitmap(bitmap,cameraView.getWidth(),cameraView.getHeight(),false);
+                cameraView.stop();;
+
+
+                runFaceDetector(bitmap);
+
+            }
+
+            @Override
+            public void onVideo(CameraKitVideo cameraKitVideo) {
+
+            }
+        });
+
     }
+
+    private void runFaceDetector(Bitmap bitmap) {
+
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+        FirebaseVisionFaceDetectorOptions options = new FirebaseVisionFaceDetectorOptions.Builder()
+                .build();
+
+        FirebaseVisionFaceDetector detector = FirebaseVision.getInstance()
+                .getVisionFaceDetector();
+        detector.detectInImage(image)
+                .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionFace>>() {
+                    @Override
+                    public void onSuccess(List<FirebaseVisionFace> firebaseVisionFaces) {
+                        processFaceResult(firebaseVisionFaces);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+
+    private void processFaceResult(List<FirebaseVisionFace> firebaseVisionFaces) {
+         
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        cameraView.start();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        cameraView.stop();
+
+    }
+
 }
