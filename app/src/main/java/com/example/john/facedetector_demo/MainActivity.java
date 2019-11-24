97fd2638e29633bbe27e834628f6a82b6878ea48
@joekifle <joey.kifle@gmail.com>
2019-11-24 15:56:58 +0300

    Implement interface

---
+++ app/src/main/java/com/example/john/facedetector_demo/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.john.facedetector_demo;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.john.facedetector_demo.Helper.GraphicOverlay;
+import com.wonderkiln.camerakit.CameraKitError;
+import com.wonderkiln.camerakit.CameraKitEvent;
+import com.wonderkiln.camerakit.CameraKitEventListener;
+import com.wonderkiln.camerakit.CameraKitImage;
+import com.wonderkiln.camerakit.CameraKitVideo;
+import com.wonderkiln.camerakit.CameraView;
+
+import dmax.dialog.SpotsDialog;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // init view
+    }
+}
